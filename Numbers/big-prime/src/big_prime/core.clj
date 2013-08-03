75bd5a121da026849d8953e1964afc4643f17eaa
Brian Beckman <bc.beckman@gmail.com>
2013-08-03 15:33:19 -0700

    Commentary bugs

--- Numbers/big-prime/src/big_prime/core.clj
+++ Numbers/big-prime/src/big_prime/core.clj
@@ -4,10 +4,14 @@
 
 (set! *warn-on-reflection* true)
 
+;;; It's unclear whether ^clojure.lang.BigInt type hints actually
+;;; improve perf. TODO: Use Criterium library to profile.
+
 (defn sum
   ([] 0)
   ([x] x)
-  ([x & more] (+ x (apply sum more))))
+  ([x & more]
+     (+ x (apply sum more))))
 
 (defn nt-average
   "Number-theoretic mean using `quot' instead of `/', which latter produces rationals"
@@ -27,17 +31,11 @@
 (defn- nt-improve
   "Improve a guess of a square root by number-theoretic average with the quotient of the guess with the target: an adaptation of Newton's method to the integer domain."
   [guess x]
-  #_(pdump guess)
-  #_(pdump (quot x guess))
-  #_(pdump (nt-average guess (quot x guess)))
   (nt-average guess (quot x guess)))
 
 (defn- good-enough?
   "A guess is good enough if its square is lte the target and the square of its increment is gt the target"
   [guess x]
-  #_(pdump (square guess))
-  #_(pdump (<= (square guess) x))
-  #_(pdump (>  (square (inc guess)) x))
   (and
    (<= (square guess) x)
    (>  (square (inc guess)) x)))
@@ -69,67 +67,8 @@
          ds) ;; in case the drop-while returns empty (all zeros)
        )))))
 
-(defn jbig-sum
-  ([] BigInteger/ZERO)
-  ([^BigInteger x] x)
-  ([^BigInteger x ^BigInteger & more] (.add x (apply jbig-sum more))))
+(defn make-partition-book-ends [x])
 
-(defn jbig-inc [^BigInteger x] (.add x BigInteger/ONE))
-(defn jbig-dec [^BigInteger x] (.subtract x BigInteger/ONE))
 
-(defn jbig-average
-  ([] BigInteger/ZERO)
-  ([^BigInteger x] x)
-  ([^BigInteger x ^BigInteger & more]
-     (.divide (.add x (apply jbig-sum more))
-              (BigInteger. (str (+ 1 (count more)))))))
-
-(defn jbig-lt [^BigInteger x ^BigInteger y] (== -1 (.compareTo x y)))
-(defn jbig-le [^BigInteger x ^BigInteger y] (or (.equals x y) (jbig-lt x y)))
-(defn jbig-gt [^BigInteger x ^BigInteger y] (==  1 (.compareTo x y)))
-(defn jbig-ge [^BigInteger x ^BigInteger y] (or (.equals x y) (jbig-gt x y)))
-
-(defn jbig-abs [^BigInteger x] (if (jbig-lt x BigInteger/ZERO) (.negate x) x))
-
-(defn jbig-square [^BigInteger x] (.multiply x x))
-
-(defn jbig-improve [^BigInteger guess ^BigInteger x]
-  (jbig-average guess (.divide x guess)))
-
-(defn jbig-good-enough? [^BigInteger guess ^BigInteger x]
-  (and
-   (jbig-le (jbig-square guess) x)
-   (jbig-gt (jbig-square (jbig-inc guess)) x)))
-
-(defn jbig-try-sqrt [^BigInteger guess ^BigInteger x]
-  (if (jbig-good-enough? guess x)
-    guess
-    (recur (jbig-improve guess x) x)))
-
-(defn jbig-sqrt [^BigInteger x] (jbig-try-sqrt BigInteger/ONE x))
-
-(defn ^BigInteger jbig-rand [digits]
-  (BigInteger. (apply str (take digits (repeatedly #(rand-int 10))))))
-
-;; I could just live with "str" of everything, since "str" is
-;; idempotent on Strings, but we may want more interesting coercions
-;; later on; also it's not frugal to str and then unstr something
-;; that's already a jbig-integer, and we want jbig-integer idempotent on
-;; jbig-integers.
-(defmulti jbig-integer type)
-(defmethod jbig-integer (type "") [string] (BigInteger. string))
-(defmethod jbig-integer (type BigInteger/ZERO) [jbig-i] jbig-i)
-(defmethod jbig-integer :default  [thing]  (BigInteger. (str thing)))
-
-(defn jbig-pos?     [^BigInteger j] (jbig-gt  j BigInteger/ZERO))
-(defn jbig-neg?     [^BigInteger j] (jbig-lt  j BigInteger/ZERO))
-(defn jbig-non-pos? [^BigInteger j] (jbig-le  j BigInteger/ZERO))
-(defn jbig-non-neg? [^BigInteger j] (jbig-ge  j BigInteger/ZERO))
-(defn jbig-zero?    [^BigInteger j] (.equals j BigInteger/ZERO))
-(defn jbig-sign     [^BigInteger j] (cond
-                                     (jbig-pos? j)  1
-                                     (jbig-neg? j) -1
-                                     :else         0
-                                     ))
 
 
--- Numbers/big-prime/test/big_prime/core_test.clj
+++ Numbers/big-prime/test/big_prime/core_test.clj
@@ -1,9 +1,9 @@
-;; If running this from nrepl in emacs, be sure to C-c M-j in a
-;; buffer visiting THIS file, the test file, and not in a buffer
-;; visiting the core file. That way, the keyboard shortcut C-c C-,
+;; If running this from nrepl in emacs, be sure to C-c M-j in a buffer
+;; visiting the core file and NOT in a buffer visiting THIS file. That
+;; way, the keyboard shortcut C-c C-, in a buffer visiting this file
 ;; will run the tests. Due to an integration gap in nrepl and
 ;; leiningen, that keystroke combination will not work if you start
-;; nrepl from a buffer visiting the core file.
+;; nrepl from a buffer visiting the test file.
 
 (ns big-prime.core-test
   (:import java.util.Random)
@@ -35,6 +35,10 @@
       time
       pdump
       )
+
+  (testing "Partitioning big range into P buckets"
+    )
+
   )
 
 (deftest integer-operation-tests
@@ -77,91 +81,4 @@
     (is (thrown? ArithmeticException (/ 0 0))))
   )
 
-(deftest jbig-integer-sqrt
-  (->
-   (testing "Invertibility of jbig-sqrt and jbig-square; also tests jbig-average, jbig-le, and private functions."
-     (let [i (pdump (jbig-rand 100))
-           j (jbig-sqrt i)
-           q (jbig-square i)
-           s (jbig-sqrt q)]
-       (is (.equals s i))
-       (is (jbig-le (jbig-square j) i))
-       (is (jbig-ge (jbig-square (jbig-inc j)) i))
-       ))
-   time
-   pdump
-   ))
 
-(deftest jbig-integer-interop-tests
-  
-  (testing "Interop with java.math.BigInteger"
-    (is (= (type BigInteger/ONE) (type (jbig-sum))))
-    (is (= (type BigInteger/ONE) (type (jbig-sum BigInteger/ONE))))
-    (is (= (type BigInteger/ONE) (type (jbig-sum BigInteger/ONE BigInteger/ONE)))))
-
-  (testing "jbig-inc and jbig-dec"
-    (is (.equals (jbig-inc BigInteger/ZERO) (BigInteger/ONE)))
-    (is (.equals (jbig-dec BigInteger/ONE)  (BigInteger/ZERO)))
-    (let [r (Random.)
-          h (jbig-rand 100)]
-      (is (.equals (jbig-inc (jbig-dec h)) h))
-      (is (.equals (jbig-dec (jbig-inc h)) h)))
-    )
-
-  (testing "String representations"
-    (is (= (str BigInteger/ZERO)          "0"))
-    (is (= (str BigInteger/ONE)           "1"))
-    (is (= (str (jbig-inc BigInteger/ONE)) "2"))
-    (is (= (str (BigInteger. "999999999999999")) "999999999999999")))
-
-  (testing "Coercions"
-    (is (= (str (jbig-integer "9999")) "9999"))
-    (is (= (str (jbig-integer 9999)) "9999"))
-    (is (= (str (jbig-integer -9999)) "-9999"))
-    (is (= (str (jbig-integer 9999N)) "9999"))
-    (is (= (str (jbig-integer -9999N)) "-9999"))
-    (is (= (str (jbig-integer 9999M)) "9999"))
-    (is (= (str (jbig-integer -9999M)) "-9999"))
-    (is (= (str (jbig-integer -9999999999999999)) "-9999999999999999"))
-    (is (= (str (jbig-integer -9999999999999999N)) "-9999999999999999"))
-    (is (= (str (jbig-integer -9999999999999999M)) "-9999999999999999")))
-
-  (testing "Idempotency"
-    (is (.equals (jbig-integer BigInteger/ZERO) BigInteger/ZERO))
-    (is (.equals (jbig-integer BigInteger/ONE)  BigInteger/ONE))
-    (let [r (Random.)
-          h (jbig-rand 100)]
-      (is (.equals (jbig-integer h) h))
-      (is (.equals (jbig-integer h) h))))
-  )
-
-
-(deftest jbig-integer-operations
-
-  (testing "big equivalence classes"
-    (let [bm1 (jbig-dec BigInteger/ZERO)]
-      (is      (jbig-pos? BigInteger/ONE))
-      (is (not (jbig-pos? BigInteger/ZERO)))
-      (is (not (jbig-pos? bm1)))
-
-      (is (not (jbig-neg? BigInteger/ONE)))
-      (is (not (jbig-neg? BigInteger/ZERO)))
-      (is      (jbig-neg? bm1))
-
-      (is      (jbig-non-neg? BigInteger/ONE))
-      (is      (jbig-non-neg? BigInteger/ZERO))
-      (is (not (jbig-non-neg? bm1)))
-
-      (is (not (jbig-non-pos? BigInteger/ONE)))
-      (is      (jbig-non-pos? BigInteger/ZERO))
-      (is      (jbig-non-pos? bm1))
-
-      (is (not (jbig-zero? BigInteger/ONE)))
-      (is      (jbig-zero? BigInteger/ZERO))
-      (is (not (jbig-zero? bm1)))
-
-      (is (=  1 (jbig-sign BigInteger/ONE)))
-      (is (=  0 (jbig-sign BigInteger/ZERO)))
-      (is (= -1 (jbig-sign bm1)))))
-
-  )
