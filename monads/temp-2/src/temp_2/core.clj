f6ff546cd8f78a38efff4055ece042a79b220545
bbeckman <bbeckman@amazon.com>
2013-09-08 22:21:38 -0700

    update draft

--- monads/temp-2/src/temp_2/core.clj
+++ monads/temp-2/src/temp_2/core.clj
@@ -56,45 +56,41 @@
             (if (randomly-error) {:error "auth errored"}
                 {:auth-token "John's credentials"})))
 (defn- read-database [auth-token]
-  (with-em
-    (m-bind auth-token
-            (fn [_] (m-result (if (randomly-error) {:error "database errored"}
-                                 {:name "John", :PO 421357}))))))
+  (em-blind auth-token
+            (if (randomly-error) {:error "database errored"}
+                {:name "John", :PO 421357})))
 (defn- call-web-service [database-results]
-  (with-em
-    (m-bind database-results
-            (fn [_] (m-result (if (randomly-error) {:error "ws1 errored"}
-                                 [{:item "camera"}, {:item "shoes"}]))))))
+  (em-blind database-results
+            (if (randomly-error) {:error "ws1 errored"}
+                [{:item "camera"}, {:item "shoes"}])))
 (defn- filter-ws [web-service-call-results]
-  (with-em
-    (m-bind web-service-call-results
-            (fn [_] (m-result (if (randomly-error) {:error "filter errored"}
-                                 [{:item "camera"}]))))))
+  (em-blind web-service-call-results
+            (if (randomly-error) {:error "filter errored"}
+                [{:item "camera"}])))
 (defn- call-other-web-service [database-results]
-  (with-em
-    (m-bind database-results
-            (fn [_] (m-result (if (randomly-error) {:error "ws2 errored"}
-                                 [{:price 420.00M}]))))))
+  (em-blind database-results
+            (if (randomly-error) {:error "ws2 errored"}
+                [{:price 420.00M}])))
 (defn- combine [filtered-web-service-results
-               other-web-service-call-results]
+                other-web-service-call-results]
   (with-em
     (m-bind filtered-web-service-results
-            (fn [m-filtered]
-              (m-result (if (randomly-error) {:error "combine errored"}
-                            (m-bind other-web-service-call-results
-                                    (fn [m-other]
-                                      (concat m-filtered
-                                              m-other)))))))))
+            (fn [filtered-val]
+              (if (randomly-error)
+                (m-result {:error "combine errored"})
+                (m-bind other-web-service-call-results
+                        (fn [other-ws-val]
+                          (m-result (concat filtered-val
+                                            other-ws-val)))))))))
 
 (println
- (with-monad if-not-error-m
-   (let [db-results
-         (-> (computation)
-             authorize
-             read-database)]
-     (-> db-results
-         call-web-service
-         filter-ws
-         (combine (call-other-web-service db-results))))
-   ))
+ (let [db-results
+       (-> (computation)
+           authorize
+           read-database)]
+   (-> db-results
+       call-web-service
+       filter-ws
+       (combine (call-other-web-service db-results))))
+ )
 
