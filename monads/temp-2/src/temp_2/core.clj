0356a3792b7058b2953c755d7303af91814a5226
bbeckman <bbeckman@amazon.com>
2013-09-12 07:44:12 -0700

    update draft

--- monads/temp-2/src/temp_2/core.clj
+++ monads/temp-2/src/temp_2/core.clj
@@ -76,6 +76,55 @@
         {:error "combine errored"}
         (concat filtered-ws-results-val
                 other-ws-results-val)))))
+
+
+(defn- computation []
+  (with-em-result
+    {:computation "computation"}))
+
+(defn- authorize [computation]
+  (with-em-result
+    (merge computation
+           (if (randomly-error)
+             {:error "auth errored"}
+             {:auth-token "John's credentials"}))))
+
+(defn- read-database [auth-token]
+  (with-em-result
+    (merge auth-token
+           (if (randomly-error)
+             {:error "database errored"}
+             {:name "John", :PO 421357}))))
+
+(defn- call-web-service [database-results]
+  (with-em-result
+    (merge database-results
+           (if (randomly-error)
+             {:error "ws1 errored"}
+             {:items [{:item "camera"}, {:item "shoes"}]}))))
+
+(defn- filter-ws [web-service-call-results]
+  (with-em-result
+    (merge web-service-call-results
+           (if (randomly-error)
+             {:error "filter errored"}
+             {:items [{:item "camera"}]}))))
+
+(defn- call-other-web-service [database-results]
+  (with-em-result
+    (merge database-results
+           (if (randomly-error)
+             {:error "ws2 errored"}
+             {:prices [{:price 420.00M}]}))))
+
+(defn- combine [other-ws-results-val]
+  (fn [filtered-ws-results-val]
+    (merge other-ws-results-val filtered-ws-results-val
+           (with-em-result
+             (if (randomly-error)
+               {:error "combine errored"}
+               {})))))
+
 #_(println
  (domonad if-not-error-m
           [a1 (computation)
@@ -104,20 +153,3 @@
         (combine (=>> db-results
                       call-other-web-service)))))
 
-#_(println
- (let [db-results
-       (with-monad if-not-error-m
-         ((m-chain [authorize
-                    read-database])
-          (computation)))
-       ]
-   (with-monad if-not-error-m
-     ((m-chain [call-web-service
-                filter-ws
-                (combine
-                 (with-monad if-not-error-m
-                   ((m-chain [call-other-web-service])
-                    db-results)))
-                ])
-      db-results)))
- )
