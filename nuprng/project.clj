e5a1798fc730d04049dbcd499335ca76409769b3
Brian Beckman <bc.beckman@gmail.com>
2013-10-13 17:34:44 -0700

    update clojure version (to run in lighttable)

--- nuprng/project.clj
+++ nuprng/project.clj
@@ -4,5 +4,5 @@
   :main nuprng.core
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/math.numeric-tower "0.0.2"]])
