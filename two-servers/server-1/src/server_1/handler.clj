bab0f76644586bb5b1956bedad672180b66ba20f
Brian Beckman <bc.beckman@gmail.com>
2013-11-10 16:40:24 -0800

    beginning REST forwarding demo

--- two-servers/server-1/src/server_1/handler.clj
+++ two-servers/server-1/src/server_1/handler.clj
@@ -1,16 +1,24 @@
 (ns server-1.handler
   (:use compojure.core)
   (:require [compojure.handler :as handler]
-            [compojure.route   :as route]
-            [clojure.pprint    :as pp]))
+            [compojure.route   :as route  ]
+            [clojure.pprint    :as pp     ])
+  (:import [rx
+            Observable
+            Observer
+            subscriptions.Subscriptions
+            subjects.Subject
+            subjects.PublishSubject])
+  )
 
-(defroutes app-routes
-  (GET "/"  [] "Hello World")
-  (POST "/" []
-        (pp/pprint "in GET")
-        "Hello Post")
-  (route/resources "/")
-  (route/not-found "Not Found"))
+(let [rsub (PublishSubject/create)]
+  (defroutes app-routes
+    (GET "/"  [] "Hello World")
+    (POST "/" []
+          (pp/pprint "in GET")
+          "Hello Post")
+    (route/resources "/")
+    (route/not-found "Not Found")))
 
 (def app
   (handler/site app-routes))
