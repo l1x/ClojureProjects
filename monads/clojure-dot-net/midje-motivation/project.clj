173da53d3a8a6db27bc1a6b1d0df6113fdec22f9
bbeckman <bbeckman@amazon.com>
2013-12-19 19:46:36 -0800

    more work on the maven repo -- not working yet

--- monads/clojure-dot-net/midje-motivation/project.clj
+++ monads/clojure-dot-net/midje-motivation/project.clj
@@ -2,5 +2,7 @@
   :description "Cool new project to do things and stuff"
   :dependencies [[org.clojure/clojure    "1.5.1"]
                  [swiss-arrows           "0.6.0"]
+                 ;; [swiss-arrows           "1.0.0"]
                  [org.clojure/algo.monads "0.1.4"]]
+  :repositories {"local" ~(str (.toURI (java.io.File. "maven_repository")))}
   :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
