0be6261a3296093ddc103cf74bb9dc7345f24f5e
bbeckman <bbeckman@amazon.com>
2014-01-19 14:46:56 -0800

    add trampolines

--- cps/src/cps/core.clj
+++ cps/src/cps/core.clj
@@ -1,5 +1,8 @@
 (ns cps.core)
 
+
+;;; See the unit-test file for exercises on these definitions.
+
 ;;; From http://www.slideshare.net/borgesleonardo/continuation-passing-style-and-macros-in-clojure-jan-2012
 
 (defn *-cps [x y k]
@@ -15,13 +18,26 @@
 
 (defn fib-cps [n k]
   (letfn [(cont [n1]
-            (fib-cps
-             (- n 2)
-             (fn [n2] (k (+ n1 n2)))
+            (fib-cps                    ; expect StackOverflowError here
+             (- n 2)                    ; n
+             (fn [n2] (k (+ n1 n2)))     ; k
              ))]
     (if (<= n 1)
       (k n)
       (recur (- n 1) cont))))
+
+
+(defn mk-cps [accept? end-value kend knext]
+  (fn [n]
+    ((fn [n k]
+       (if (accept? n)
+         (k end-value)
+         (recur (dec n) (fn [v] (k (knext v n)))))
+       )
+     n kend)))
+
+(def fac0 (mk-cps zero? 1N identity #(*' %1 %2)))
+(def tri0 (mk-cps zero? 1N dec      #(+' %1 %2)))
 
 ;;; From http://pramode.net/clojure/2010/05/08/clojure-trampoline/
 
@@ -36,3 +52,41 @@
   (if (= n 0)
     0
     #(funa (dec n))))
+
+(defn func [n]
+  (if (= n 0)
+    0
+    (func (dec n))))
+
+(defn fund [n]
+  (if (= n 0)
+    0
+    #(fund (dec n))))
+
+(defn fune [n k]
+  (if (= n 0)
+    (k 0)
+    (fune (dec n) k)))
+
+(defn funf [n k]
+  (if (= n 0)
+    (k 0)
+    #(funf (dec n) k)))
+
+(defn facr [n]
+  (reduce *' (drop 1 (range (inc n)))))
+
+(defn faca [n k]
+  (if (= n 0N)
+    (k 1N)
+    (*' n (faca (dec n) k))))
+
+(defn facb [n a k]
+  (if (= n 0N)
+    (k a)
+    #(facb (dec n) (*' a n) k)))
+
+(defn facc [n k]
+  (if (= n 0N)
+    (k 1N)
+    #(facc (dec n) (fn [n-] (k (*' n n-))))))
--- cps/test/cps/core_test.clj
+++ cps/test/cps/core_test.clj
@@ -8,8 +8,35 @@
 
 (deftest fib-test
   (testing "fib-cps"
-    (is (= 55 (fib-cps 10 identity)))))
+    (is (= 55 (fib-cps 10 identity)))
+    (is (= 610 (fib-cps 15 identity)))
+    (is (thrown? StackOverflowError (fib-cps 20 identity)))
+    ))
+
+(deftest mk-cps-test
+  (is (= (facr 10000N) (fac0 10000N)))
+  (is (= 50005000N (tri0 10000N))))
 
 (deftest trampoline-test
   (is (= 0 (trampoline funa 42)))
-  (is (= 0 (trampoline funa 100000))))
+  (is (= 0 (trampoline funa 100000)))
+  (is (= 5 (+-cps 2 3 identity)))
+  (is (= 5 (trampoline #(+-cps 2 3 identity))))
+  (is (= 5 (trampoline #(+-cps 2 3 %) identity)))
+  (is (= 5 (trampoline #(+-cps 2 3 (fn [a] (identity a))))))
+  (is (= 5 (trampoline #(+-cps 2 3 (fn [a] (trampoline identity a))))))
+  (is (= 0 (func 100)))
+  (is (thrown? StackOverflowError (func 10000)))
+  (is (= 0 (trampoline fund 10000)))
+  (is (= 0 (fune 100 identity)))
+  (is (thrown? StackOverflowError (fune 10000 identity)))
+  (is (= 0 (trampoline funf 100 identity)))
+  (is (= 0 (trampoline funf 10000 identity)))
+  (is (= 720N (faca 6N identity)))
+  (is (thrown? StackOverflowError (faca 10000N identity)))
+  (is (= 720N (trampoline facb 6N 1N identity)))
+  (is (= (facr 10000N) (trampoline facb 10000N 1N identity)))
+  (is (= 1N (trampoline facc 0N identity)))
+  (is (= 720N (trampoline facc 6N identity)))
+  (is (thrown? StackOverflowError (trampoline facc 10000N identity)))
+  )
