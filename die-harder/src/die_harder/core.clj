49a6f6a50af41b5cca3257371531389f5ef5f3f9
bbeckman <bbeckman@amazon.com>
2014-02-19 08:41:26 -0800

    die-harder, skew-zip

---
+++ die-harder/src/die_harder/core.clj
@@ -0,0 +1,15 @@
+(ns die-harder.core)
+
+(def jugs [0 0])
+(def caps [5 3])
+
+(defn sum [xs])
+
+(defn pour [from to]
+  (let [poured (min ())])
+  )
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
