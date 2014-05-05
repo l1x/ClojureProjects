df5c7515d79bed3bafa7c43da1fdc072c6418ae2
bbeckman <bbeckman@amazon.com>
2014-05-05 16:38:56 -0700

    add binary search; transition from hashmap to vector

--- nuprng/src/nuprng/core.clj
+++ nuprng/src/nuprng/core.clj
@@ -9,8 +9,9 @@
 ;;; 'G' with proportions P = (5, 8, 13, 0, 6, 4, 3). Generate characters
 ;;; randomly and statistically in those proportions. Show that your
 ;;; solution has the given statistics. You may use a uniformly
-;;; distributed floating-point pseudo-random number generator such as
-;;; Unix "rand." Your randoms need not be cryptographically strong.
+;;; distributed floating-point, [0, 1) pseudo-random number generator
+;;; such as Unix "rand." Your randoms need not be cryptographically
+;;; strong.
 ;;;
 ;;; Your code should handle the general case where the number of
 ;;; outcomes is N and the given proportions are large integers. The
@@ -39,12 +40,68 @@
          (clojure.pprint/pprint x#)
          x#)))
 
+(def outcome
+  "Get the outcome from a outcome-frequency pair."
+  first)
+
+(def frequency
+  "Get the frequency from an outcome-frequency pair."
+  second)
+
+(defn total
+  "The sum of frequencies in a distribution, which is a sequence of
+pairs of outcomes and frequencies."
+  [frqs] (apply + (map frequency frqs)))
+
+(defn rand-int2
+  "Produce a random integer from 0 (inclusive) to mac (exclusive). Note
+that Clojure hhas a built-in \"rand-int\", but it is not allowed in this
+program as a pedogogical precondition \"... You may use a uniformly
+distributed floating-point, [0, 1) pseudo-random number generator such
+as Unix 'rand.'"
+  [mac] (->> (rand)
+             (* mac)
+             int))
+
+(defn sample-logarithmically
+  "Produce n samples of the outcome-frequency vector by binary
+searching the cumulative distribution function; this is O(N)-space,
+O(log N)-time. "
+  [n frqs-]
+  (let [frqs (filter (comp pos? frequency) frqs-)
+        cdf  (vec (reductions (fn [results pair]
+                                [(outcome pair)
+                                 (+ (frequency results)
+                                    (frequency pair))])
+                              (first frqs)
+                              (rest frqs)))
+        sum  (total frqs)
+        len  (count frqs)
+        mid  (fn [lo hi] (int (/ (+ lo hi) 2)))]
+    (map (fn [target]
+           (loop [l 0, h (dec len)]
+             (let [i (mid l h), v (frequency (cdf i)), o (outcome (cdf i))]
+#_(clojure.pprint/pprint {:l l :h h :i i :t target :cdf cdf :v v :o o})
+               (if (>= l h) o
+                   (cond
+                    (>= target v)
+                    (let [nx (cdf (inc i))
+                          nv (frequency nx)
+                          no (outcome   nx)]
+#_(clojure.pprint/pprint {:nx nx :nv nv :no no})
+                      (if (< target nv) no
+                          (recur (inc i) h)))
+
+                    (< target v)
+                    (recur l i)
+                    )))))
+         (repeatedly n (fn [] (rand-int2 sum))))))
+
 (defn sample-linearly
-  "Produce n samples of the outcome-frequency hashmap by linear
+  "Produce n samples of the outcome-frequency vector by linear
 searching; this is O(N)-space, O(N)-time. "
-  [n outcome-freqency-hashmap]
-  (let [frqs (map identity outcome-freqency-hashmap)
-        sum  (total frqs)]
+  [n frqs-]
+  (let [frqs (filter (comp pos? frequency) frqs-), sum  (total frqs)]
     (map (fn [target]
            (loop [candidates frqs
                   so-far (frequency (first candidates))]
@@ -59,11 +116,7 @@ searching; this is O(N)-space, O(N)-time. "
                       ;; An alternative implementation is to subtract
                       ;; the current frequency from the target.
                       (+ so-far (frequency (second candidates)))))))
-         (map (fn [_] (->> (rand)
-                          (* sum)
-                          mathEx/floor
-                          int))
-              (range n)))))
+         (repeatedly n (fn [] (rand-int2 sum))))))
 
 
 ;;; The following presents an O(N)-space, O(1)-time solution using
@@ -76,22 +129,6 @@ searching; this is O(N)-space, O(N)-time. "
 ;;; increased so that the new total is disvisible both by N, so that the
 ;;; new bins will all contain the same total amounts; and by S, the sum
 ;;; of the original counts, so that that the old counts
-
-;;; This is the form of data returned by "frequencies,"
-(def loaded-die {:A 37, :B 0, :C 17, :D 5, :E 12, :F 11 :G 44})
-
-(defn total
-  "The sum of frequencies in a distribution, which is a sequence of
-pairs of outcomes and frequencies."
-  [frqs] (apply + (map frequency frqs)))
-
-(def outcome
-  "Get the outcome from a outcome-frequency pair."
-  first)
-
-(def frequency
-  "Get the frequency from an outcome-frequency pair."
-  second)
 
 (defn N
   "The number of outcomes in a distribution."
@@ -126,10 +163,10 @@ redistribution."
 
 (defn fill-shortest
   "Given a target height, a hashmap of :filled and :remaining bins, and
-a sequence of outcome-frequency pairs, produce a new hashmap of :filled
-and :remaining bins; the :filled item of that hashmap should be a vector
-of :home and :other hashmaps, where each one specifies the two colors in
-the bin after redistribution."
+a vector of outcome-frequency pairs, produce a new hashmap of :filled
+and :remaining bins. The :filled item of that hashmap should be a vector
+of hashmaps, each specifying :home and :other, the two outcomes in the
+bin after redistribution."
   [target filled frqs]
   (let [sorted   (sort-by frequency frqs)
         tallest  (last    sorted)
@@ -142,11 +179,10 @@ the bin after redistribution."
      (if (empty? to-do)
        to-do
        (conj (drop-last to-do)
-             ;; make a new outcome-frequency pair.
              [(outcome tallest) (- (frequency tallest) deficit)]))}))
 
 (defn redistribute
-  "Given a target height for each bin and a sequence of beginning
+  "Given a target height for each bin and a vector of beginning
 outcome-frequency pairs, produce a vector of redistributed frequencies,
 where each new bin is a hash map of the new :home outcome-frequency pair
 and the new frequency of the :other outcome-frequency pair. "
@@ -172,10 +208,10 @@ outcome from a sequence of redistributed frequencies."
 (defn sample-walker
   "Takes n random samples from a hashmap of outcomes and frequencies;
 preprocessing aside, this is O(N)-space, O(1)-time."
-  [n outcome-freqency-hashmap]
+  [n outcome-freqency]
   (let [redistributed (redistribute
-                       (H outcome-freqency-hashmap)
-                       (augmented outcome-freqency-hashmap))
+                       (H outcome-freqency)
+                       (augmented outcome-freqency))
         ansatz (first redistributed)
         target (+ (frequency (:home  ansatz))
                   (frequency (:other ansatz)))]
@@ -185,13 +221,15 @@ preprocessing aside, this is O(N)-space, O(1)-time."
 ;;; The following is no substitute for unit tests, but it produces a
 ;;; nice printed result of an experiment.
 
+(def loaded-die [[:A 37] [:B 0] [:C 17] [:D 5] [:E 12] [:F 11] [:G 44]])
+
 (defn -main [] (pprint
                 (let [redis (augmented loaded-die)
                       h     (H loaded-die)
                       n     (N loaded-die)
                       s     (S loaded-die)
                       l     (L loaded-die)]
-                  {"loaded-die" loaded-die
+                  {"original distribution" loaded-die
                    ,"original count"  n
                    ,"original total"  s
                    ,"gcd count total" (mathEx/gcd n s)
@@ -211,4 +249,9 @@ preprocessing aside, this is O(N)-space, O(1)-time."
                    (time
                     (frequencies
                      (sample-linearly (* 10000 s)
+                                      loaded-die)))
+                   ,"sample-logarithmically 10000"
+                   (time
+                    (frequencies
+                     (sample-logarithmically (* 10000 s)
                                       loaded-die)))})))
